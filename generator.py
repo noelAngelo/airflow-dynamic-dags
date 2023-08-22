3c3418c69c328a840a46331c73eeb437efbf00e2
noel <born.angelonoel@gmail.com>
2023-08-22 17:56:09 +1000

    updated working dag

--- generator.py
+++ generator.py
@@ -4,11 +4,12 @@ import os
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
 env = Environment(loader=FileSystemLoader(file_dir))
-template = env.get_template('./templates/template_dag.jinja2')
 
 for filename in os.listdir(f'{file_dir}/config'):
     if filename.endswith('.yaml'):
         with open(f'{file_dir}/config/{filename}', 'r') as configfile:
             config = yaml.safe_load(configfile)
+            template_name = config['dag_template']
+            template = env.get_template(f'./templates/{template_name}.jinja2')
             with open(f'dags/{config["dag_id"]}.py', 'w') as f:
                 f.write(template.render(config))
